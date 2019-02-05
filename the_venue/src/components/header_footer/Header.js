19785cdcb5cdff66cfae5b85fc25e8bfd531ca36
viseth89 <viseth89@gmail.com>
2019-02-04 22:24:18 -0600

    working on header sidedrawer

--- the_venue/src/components/header_footer/Header.js
+++ the_venue/src/components/header_footer/Header.js
@@ -5,6 +5,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
   render() {
     return (
@@ -17,7 +19,23 @@ class Header extends Component {
             padding: '10px 0px'
           }}
         >
+          <Toolbar>
+            <div className="header_logo">
+              <div className="font_righteous header_logo_venue">The Venue</div>
+              <div className="header_logo_title">Musical Events</div>
+            </div>
 
+            <IconButton
+            
+            aria-label="Menu"
+            color='inherit'
+            onClick={() => console.log('open')}
+            >
+
+
+            </IconButton>
+
+          </Toolbar>
         </AppBar>
 
     )
