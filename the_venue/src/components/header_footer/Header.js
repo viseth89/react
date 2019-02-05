7623a32046533d125a105d83e824730ad5e473c0
viseth89 <viseth89@gmail.com>
2019-02-04 18:31:59 -0600

    geez heading

---
+++ the_venue/src/components/header_footer/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/core/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+class Header extends Component {
+  render() {
+    return (
+
+        <AppBar
+          position='fixed'
+          style={{
+            backgroundColor:'#2f2f2f',
+            boxShadow: 'none',
+            padding: '10px 0px'
+          }}
+        >
+
+        </AppBar>
+
+    )
+  }
+}
+
+export default Header;
